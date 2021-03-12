77448f6109fb36d39add3de969c50abae4064206
Adrian <adrian_zinko@op.pl>
2021-03-12 23:12:53 +0100

    6.3 Prepare class for tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,26 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.collection.OddNumbersExterminator;
 import com.kodilla.testing.user.SimpleUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestingMain {
     public static void main(String[] args) {
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+
+        List<Integer> integerArrayList = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            integerArrayList.add(i);
+        }
+
+        List<Integer> oddNumbers = oddNumbersExterminator.exterminate(integerArrayList);
+
+        for (Integer num : oddNumbers) {
+            System.out.print(num + " ");
+        }
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.Test;
+
+public class CollectionTestSuite {
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
