84f6b3d1e69aaa89a7e9dda9eecb0dad85d658a7
Adrian <adrian_zinko@op.pl>
2021-03-16 02:19:56 +0100

    TDD: Implement methods based on tests

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,19 +1,52 @@
 package com.kodilla.testing.shape;
 
-public class ShapeCollector {
-    public void addFigure(Shape shape) {
-        // silence is golden
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeCollector implements Shape {
+
+    private List<Shape> figures = new ArrayList<>();
+
+    public void addFigure(Shape theShape) {
+        figures.add(theShape);
     }
 
-    public boolean removeFigure(Shape shape) {
-        return true;
+    public boolean removeFigure(Shape theShape) {
+        boolean result = false;
+        if (figures.contains(theShape)) {
+            figures.remove(theShape);
+            result = true;
+        }
+        return result;
     }
 
-    public Shape getFigure(int n) {
+    public Shape getFigure(int figureNumber) {
+        if (figureNumber >= 0 && figureNumber < figures.size()) {
+            return figures.get(figureNumber);
+        }
         return null;
     }
 
-    public void showFigures() {
-        // silence is golden
+    public int getFiguresQuantity() {
+        return figures.size();
+    }
+
+    public List<String> showFigures() {
+        List<String> figureNames = new ArrayList<>();
+
+        for (Shape figure: figures) {
+            figureNames.add(figure.getShapeName());
+        }
+        return figureNames;
+    }
+
+    @Override
+    public String getShapeName() {
+        return null;
+    }
+
+    @Override
+    public double getField() {
+        return 0;
     }
 }
