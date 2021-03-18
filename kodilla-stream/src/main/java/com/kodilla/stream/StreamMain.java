23fc21cdf88caec7160db29b5fa206cb2dd994c3
Adrian <adrian_zinko@op.pl>
2021-03-18 18:15:00 +0100

    7.1 Practice lambda expressions and reference methods

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.Decorator;
-import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
