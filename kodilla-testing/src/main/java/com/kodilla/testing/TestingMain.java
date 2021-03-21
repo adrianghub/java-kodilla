1266e3da43084d452e0eff822c4947c77d9ad2bc
Adrian <adrian_zinko@op.pl>
2021-03-21 23:15:11 +0100

    Cleanup: Remove unused imports

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,12 +1,5 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.collection.OddNumbersExterminator;
-import com.kodilla.testing.user.SimpleUser;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class TestingMain {
     public static void main(String[] args) {
 
