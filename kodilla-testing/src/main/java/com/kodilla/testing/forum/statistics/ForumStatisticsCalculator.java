a83b1e15534ac9558c911a16d64051b8ea9f9e42
Adrian <adrian_zinko@op.pl>
2021-03-24 21:41:04 +0100

    6.7. Implement method for calculating forum data

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatisticsCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatisticsCalculator.java
@@ -1,40 +1,35 @@
 package com.kodilla.testing.forum.statistics;
 
-import com.kodilla.testing.forum.ForumPost;
-import com.kodilla.testing.forum.ForumUser;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ForumStatisticsCalculator implements Statistics {
+public class ForumStatisticsCalculator {
 
     private int quantityOfUserNames;
     private int quantityOfPostBodies;
     private int quantityOfPosts;
     private int quantityOfComments;
-    private int averagePostsCount;
-    private int averageCommentsCountByUser;
-    private int averageCommentsCountByPost;
+    private double averagePostsCount;
+    private double averageCommentsCountByUser;
+    private double averageCommentsCountByPost;
 
-    private final List<ForumUser> forumUsers = new ArrayList<>();
-    private final List<ForumPost> forumPosts = new ArrayList<>();
-    private final List<String> forumUsersNames = new ArrayList<>();
-    private final List<String> forumPostsBodies = new ArrayList<>();
     private Statistics statistics;
 
     public ForumStatisticsCalculator(Statistics statistics) {
         this.statistics = statistics;
     }
 
-    public void calculateAdvStatistics(Statistics statistics) {
-        this.quantityOfUserNames = statistics.usersNames().size();
-        this.quantityOfPostBodies = statistics.postBodies().size();
-        this.quantityOfPosts = statistics.postsCount();
-        this.quantityOfComments = statistics.commentsCount();
+    public void calculateAdvStatistics() {
+        quantityOfUserNames = statistics.usersNames().size();
+        quantityOfPostBodies = statistics.postBodies().size();
+        quantityOfPosts = statistics.postsCount();
+        quantityOfComments = statistics.commentsCount();
 
-        this.averagePostsCount = quantityOfPosts / quantityOfUserNames;
-        this.averageCommentsCountByUser = quantityOfComments / quantityOfUserNames;
-        this.averageCommentsCountByPost = quantityOfComments / quantityOfPostBodies;
+        if (quantityOfUserNames > 0) {
+            averagePostsCount = quantityOfPosts / quantityOfUserNames;
+            averageCommentsCountByUser = quantityOfComments / quantityOfUserNames;
+        }
+
+        if (quantityOfPosts > 0) {
+            averageCommentsCountByPost = quantityOfComments / quantityOfPosts;
+        }
     }
 
     public void showStatistics() {
@@ -43,28 +38,12 @@ public class ForumStatisticsCalculator implements Statistics {
                         "Posts:" + quantityOfPosts +
                         "Comments: " + quantityOfComments +
                         "Average posts quantity: " + averagePostsCount +
-                        "Average posts by user quantity: " + averageCommentsCountByUser +
+                        "Average posts by us    er quantity: " + averageCommentsCountByUser +
                         "Average posts by comments quantity: " + averageCommentsCountByPost);
-    }
-
-    public void addForumUser(ForumUser forumUser) {
-        forumUsers.add(forumUser);
-    }
-
-    @Override
-    public List<String> usersNames() {
-        for (ForumUser user : forumUsers) {
-            forumUsersNames.add(user.getName());
-        }
-        return forumUsersNames;
     }
 
     public int getQuantityOfUserNames() {
         return quantityOfUserNames;
-    }
-
-    public int getQuantityOfPostBodies() {
-        return quantityOfPostBodies;
     }
 
     public int getQuantityOfPosts() {
@@ -75,42 +54,15 @@ public class ForumStatisticsCalculator implements Statistics {
         return quantityOfComments;
     }
 
-    public int getAveragePostsCount() {
+    public double getAveragePostsCount() {
         return averagePostsCount;
     }
 
-    public int getAverageCommentsCountByUser() {
+    public double getAverageCommentsCountByUser() {
         return averageCommentsCountByUser;
     }
 
-    public int getAverageCommentsCountByPost() {
+    public double getAverageCommentsCountByPost() {
         return averageCommentsCountByPost;
-    }
-
-    @Override
-    public List<String> postBodies() {
-        int quantityOfAllForumComments = commentsCount();
-        for (ForumPost post : forumPosts) {
-            forumPostsBodies.add(post.getPostBody());
-        }
-        return forumPostsBodies;
-    }
-
-    @Override
-    public int postsCount() {
-        int sum = 0;
-        for (ForumUser user : forumUsers) {
-            sum += user.getPostsQuantity();
-        }
-        return sum;
-    }
-
-    @Override
-    public int commentsCount() {
-        int sum = 0;
-        for (ForumUser user : forumUsers) {
-            sum += user.getCommentsQuantity();
-        }
-        return sum;
     }
 }
