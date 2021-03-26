d42f7f2d3172ff359024b6fb57d01fb4913ab583
Adrian <adrian_zinko@op.pl>
2021-03-26 23:25:56 +0100

    8.1. Exception handling - the right way

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -2,6 +2,8 @@ package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
 
+import java.io.IOException;
+
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
